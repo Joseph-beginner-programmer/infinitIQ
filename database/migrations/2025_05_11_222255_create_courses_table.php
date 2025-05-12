679e98ddaf87eab345847a8b8bbe79337aba4678
joseph-beginner-programmer <joseptan1512@outlook.com>
2025-05-12 12:17:14 +0700

    database logic added

---
+++ database/migrations/2025_05_11_222255_create_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+
+            // Foreign Keys
+            $table->foreignId('instructor_id')->constrained('users')->onDelete('cascade'); // assuming instructors are users
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+
+            // Course Length
+            $table->integer('length_value'); // e.g., 1, 2, 3
+            $table->enum('length_unit', ['day', 'week', 'month']); // enum for standard units
+
+            // Level (optional FK or enum/string)
+            $table->enum('level', ['beginner', 'intermediate', 'advanced'])->default('beginner');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
---
+++ resources/views/pages/courses.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="title-container flex flex-col mt-5 w-full max-w-screen-sm mx-auto px-4">
+        <div class="title">
+            <h1 class="font-roboto font-bold text-[2rem]">
+                Online Course on Design and Development
+            </h1>
+            
+        </div>
+        <div class="description font-roboto flex-basis-2">
+            <p>Welcome to our online course page, where you can enhance your skills in design and development. Choose from our carefully curated selection of 10 courses designed to provide you with comprehensive knowledge and practical experience. Explore the courses below and find the perfect fit for your learning journey.</p>
+        </div>
+    </div>
+@endsection
